1a1d8cdc45ad490aeb68e1109ebdba75c5d697e0
zoniha <awaawapurin@gmail.com>
2022-04-14 00:05:56 +0900

    ✨ feat: Add main contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { VFC } from 'react';
+import { Description } from './components/PageFragments/Description/Description';
 import { Footer } from './components/PageLayout/Footer/Footer';
 import { Layout } from './components/PageLayout/Layout/Layout';
+import { TextForm } from './components/TextForm/TextForm';
 
 export const App: VFC = () => {
   return (
     <Layout>
+      <Description />
+      <TextForm />
       <Footer />
     </Layout>
   );
