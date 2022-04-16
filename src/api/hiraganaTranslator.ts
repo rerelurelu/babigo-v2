8b9c62a49835e92744ea9cfac48a20532ef4ccab
zoniha <awaawapurin@gmail.com>
2022-04-17 05:43:30 +0900

    🐛 fix: Fix bug

--- src/api/hiraganaTranslator.ts
+++ src/api/hiraganaTranslator.ts
@@ -1,25 +1,29 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { GooApiPost, GooApiResponse } from '../types/gooApi/gooApi';
 
-const GOO_API: string | undefined = process.env.GOO_API;
+//const GOO_ID: string | undefined =
+//  'fdda6af9d1880a38e8e132c9fdc225d33e37fa7c6d48d525dd33cfac6c6b3ba2';
+const GOO_ID: string = import.meta.env.VITE_GOO_ID;
 const GOO_API_RUL: string = 'https://labs.goo.ne.jp/api/hiragana';
 
-export const fetchHiragana = (inputText: string): string => {
+export const fetchHiragana = async (inputText: string): Promise<string> => {
   let data: GooApiPost = {
-    app_id: GOO_API,
+    app_id: GOO_ID,
     sentence: inputText,
     output_type: 'hiragana',
   };
 
   let hiragana: string = '';
 
-  axios
+  await axios
     .post(GOO_API_RUL, data)
     .then((res: AxiosResponse<GooApiResponse>) => {
       const convertedData = res.data;
       hiragana = convertedData.converted;
     })
-    .catch((err: AxiosError) => console.log(err));
+    .catch((_: AxiosError) => {
+      alert(`変換に失敗しました😢`);
+    });
 
   return hiragana;
 };
--- src/translator/babiTranslator.ts
+++ src/translator/babiTranslator.ts
@@ -5,36 +5,38 @@ export const babiConverter = (hiragana: string): string => {
   const textArray: string[] = [...hiragana];
 
   textArray.forEach((letter: string, idx: number) => {
-    let next_letter: string | undefined = textArray[idx + 1];
-    let prev_letter: string | undefined = textArray[idx - 1];
+    let next_letter: string = textArray[idx + 1] === undefined ? '' : textArray[idx + 1];
+    let prev_letter: string = textArray[idx - 1] === undefined ? '' : textArray[idx - 1];
 
-    if (next_letter.includes('ぁぃぅぇぉゃゅょ') && idx < textLength) {
-    } else if (letter.includes('あかがさざただなはばぱまやらわゎ')) {
+    if (next_letter.search('ぁ|ぃ|ぅ|ぇ|ぉ|ゃ|ゅ|ょ') !== -1 && idx < textLength) {
+      // Pass
+    } else if (letter.search('あ|か|が|さ|ざ|た|だ|な|は|ば|ぱ|ま|や|ら|わ|ゎ') !== -1) {
       babigo += letter + 'ば';
-    } else if (letter.includes('いきぎしじちぢにひびぴみり')) {
+    } else if (letter.search('い|き|ぎ|し|じ|ち|ぢ|に|ひ|び|ぴ|み|り') !== -1) {
       babigo += letter + 'び';
-    } else if (letter.includes('うゔくぐすずつづぬふぶぷむゆるん')) {
+    } else if (letter.search('う|ゔ|く|ぐ|す|ず|つ|づ|ぬ|ふ|ぶ|ぷ|む|ゆ|る|ん') !== -1) {
       babigo += letter + 'ぶ';
-    } else if (letter.includes('えけげせぜてでねへべぺめれ')) {
+    } else if (letter.search('え|け|げ|せ|ぜ|て|で|ね|へ|べ|ぺ|め|れ') !== -1) {
       babigo += letter + 'べ';
-    } else if (letter.includes('おこごそぞとどのほぼぽもよろを')) {
+    } else if (letter.search('お|こ|ご|そ|ぞ|と|ど|の|ほ|ぼ|ぽ|も|よ|ろ|を') !== -1) {
       babigo += letter + 'ぼ';
-    } else if (letter.includes('ぁゃ')) {
+    } else if (letter.search('ぁ|ゃ') !== -1) {
       babigo += prev_letter + letter + 'ば';
-    } else if (letter.includes('ぃ')) {
+    } else if (letter.search('ぃ') !== -1) {
       babigo += prev_letter + letter + 'び';
-    } else if (letter.includes('ぅゅ')) {
+    } else if (letter.search('ぅ|ゅ') !== -1) {
       babigo += prev_letter + letter + 'ぶ';
-    } else if (letter.includes('ぇ')) {
+    } else if (letter.search('ぇ') !== -1) {
       babigo += prev_letter + letter + 'べ';
-    } else if (letter.includes('ぉょ')) {
+    } else if (letter.search('ぉ|ょ') !== -1) {
       babigo += prev_letter + letter + 'ぼ';
-    } else if (letter.includes('ー〜') && idx < textLength) {
+    } else if (letter.search('ー|〜') !== -1 && idx < textLength) {
       babigo += letter + babigo.slice(-1);
     } else {
       babigo += letter;
     }
   });
 
+  babigo = babigo.replace(/\s+/g, '');
   return babigo;
 };
