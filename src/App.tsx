9da4a6149113955812c8e89d8671f85e7f19b0a5
zoniha <awaawapurin@gmail.com>
2022-04-17 05:43:00 +0900

    ✨ feat: Add text form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { VFC } from 'react';
 import { Description } from './components/PageFragments/Description/Description';
+import { TextForm } from './components/PageFragments/TextForm/TextForm';
 import { Footer } from './components/PageLayout/Footer/Footer';
 import { Layout } from './components/PageLayout/Layout/Layout';
-import { TextForm } from './components/TextForm/TextForm';
 
 export const App: VFC = () => {
   return (
