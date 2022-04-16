4bbce5200d34922bdf12c0ec62397c5eb8ce3740
zoniha <awaawapurin@gmail.com>
2022-04-17 05:45:12 +0900

    🎨 layout: Update

--- src/components/PageLayout/Layout/Layout.tsx
+++ src/components/PageLayout/Layout/Layout.tsx
@@ -1,6 +1,8 @@
-import { VFC, ReactNode } from 'react';
+import { VFC } from 'react';
 import styled from 'styled-components';
+
 import { Props } from '../../../types/props/props';
+import { device } from '../../../style/breakpoints';
 
 // Component style
 const StMainContainer = styled.div`
@@ -8,7 +10,13 @@ const StMainContainer = styled.div`
   color: #ffffff;
   background: linear-gradient(315deg, #fcd181 0%, #e79087 74%);
   min-height: 100vh;
-  padding-bottom: 30px;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media ${device.laptop} {
+    padding-bottom: 30px;
+  }
 `;
 
 const StTitle = styled.h1`
